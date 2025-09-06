da8a5943dad2ae2dc9f4a5e95b6f931f3656e932
Vivek-Singh-ops <vs6778373@gmail.com>
2025-09-06 17:16:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-console.log("API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
+
 
 
 const firebaseConfig = {
@@ -17,4 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 export default app;
